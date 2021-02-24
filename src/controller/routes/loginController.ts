2041ec2885a22e7cabb22fb2c8316fb24f812836
JEANTET Joey <jeantetjoey.pro@gmail.com>
2021-02-24 16:44:35 +0100

    Amélioration du controller de connexion

--- src/controller/routes/loginController.ts
+++ src/controller/routes/loginController.ts
@@ -1,17 +1,37 @@
 import controller from "../../app/controller";
 import {randomBytes} from "crypto";
+import {utilisateur} from "../../models/utilisateur.model";
 import DataStoreController from "../../extensions/dataStore/DataStoreController";
 
 export default class loginController extends controller{
-    index(){
-        console.log(this.request.headers.authorization);
-        randomBytes(256, (err, buf) => {
+    async index(){
+        let username = this.request.body.username;
+        let password = this.request.body.password;
+        let users: any = await utilisateur.query()
+            .select("username")
+            .where("username", "=", username)
+            .where("password", "=", password);
+        if(users[0] !== undefined)
+        {
+            randomBytes(256, (err, buf) => {
+                let buffer = buf.toString("hex");
+                let datastore = DataStoreController.getInstance();
+                let loggedArray = datastore.addData("logged", []);
+                loggedArray.push(buffer);
+                this.response.json(
+                    {
+                        success: true,
+                        token:  buf.toString("hex"),
+                        loggedArray: loggedArray
+                    }
+                );
+            });
+        }else{
             this.response.json(
                 {
-                    token:  buf.toString("hex")
+                    success: false
                 }
-                );
-        });
-        //this.response.render('index');
+            );
+        }
     }
 }
--- src/route.ts
+++ src/route.ts
@@ -3,6 +3,6 @@ export default [
     //{path: "/test", controller: "testController", type: "get"},
     //{path: "/session", controller: "sessionController", type: "get"}
     //{path: "/api", controller: "apiController", type: "get"},
-    {path: "/api/login", controller: "loginController", type: "get"},
+    {path: "/api/login", controller: "loginController", type: "post"},
 
 ]
