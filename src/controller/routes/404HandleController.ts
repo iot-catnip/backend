c047b3c87d9d1d07521fc055229257187a840150
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-02-25 21:03:44 +0100

    Edit: Suppression des import inutile

--- src/controller/routes/404HandleController.ts
+++ src/controller/routes/404HandleController.ts
@@ -1,7 +1,4 @@
 import controller from "../../app/controller";
-import {createHash, randomBytes} from "crypto";
-import {utilisateur} from "../../models/utilisateur.model";
-import DataStoreController from "../../extensions/dataStore/DataStoreController";
 import path from "path";
 
 export default class handle404Controller extends controller{
