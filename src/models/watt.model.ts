e5bb6ffcae081744c551704a9dc4b7746ecbacde
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-02-26 16:23:04 +0100

    Ajout des modèles et relation

---
+++ src/models/watt.model.ts
@@ -0,0 +1,53 @@
+import { Model } from 'objection';
+import knex from '../knex'
+import { subController } from '../subController'
+import {prise} from "./prise.model";
+
+Model.knex(knex)
+
+export class watt extends Model {
+    valeur!: number;
+    date_mesure!: number;
+    prise_id!: number;
+
+    static get tableName() {
+        return 'watts';
+    }
+
+    static relationMappings = {
+        prise: {
+            relation: Model.HasOneRelation,
+            modelClass: prise,
+            join: {
+                from: 'watts.prise_id',
+                to: 'prises.prise_id'
+            }
+        }
+    }
+
+    static afterInsert({ inputItems }: any) {
+        subController.index('watt', inputItems[0], 'insert')
+      }
+
+    static afterUpdate({inputItems}: any){
+    subController.index('watt', inputItems[0], 'update')
+    }
+
+    static afterDelete(){
+    subController.index('watt', {}, 'delete')
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['valeur', 'date_mesure', 'prise_id'],
+
+            properties: {
+                id: {type: 'integer'},
+                valeur: {type: 'integer'},
+                date_mesure: {type: 'integer'},
+                prise_id: {type: 'integer'}
+            }
+        }
+    }
+}
