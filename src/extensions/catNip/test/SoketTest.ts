b2179da1945ded4fd0f9b60e6f87919d32c2abb6
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-02-27 13:27:13 +0100

    New : Création d'une classe de test pour encoder et décoder les soket

---
+++ src/extensions/catNip/test/SoketTest.ts
@@ -0,0 +1,31 @@
+import {Socket,createServer} from "net";
+import CatNip from "../CatNip";
+
+//server
+createServer(function (socket:Socket) {
+    console.log("connected");
+
+    //When recive data
+    socket.on('data', function (chunk:Buffer) {
+        catnip = new CatNip()
+        catnip.decodeFrame(chunk)
+        console.log(catnip.getFrameType)
+    });
+}).listen(8080);
+
+// client
+let s = new Socket();
+s.connect(8080);
+// Create tram
+let catnip = new CatNip();
+catnip.setPacketType(CatNip.ASK_TEMPERATURE);
+catnip.encodeFrame();
+//send tram
+s.write(catnip.getFrame());
+s.end();
+
+const test = 0x12, test1 = 0x10
+const add = (a:number, b:number) => (a << Math.ceil(Math.log2(b)) + 1) + b;
+const arr = new Uint8Array([0x12,0x10])
+console.log(parseInt(test.toString(2) + test1.toString(2), 2))
+console.log(CatNip.concatenateBytes(arr))
