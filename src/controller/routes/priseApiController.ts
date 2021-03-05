1d5fa9d4774e9e444958852ea7dede742728c63f
JEANTET Joey <jeantetjoey.pro@gmail.com>
2021-03-05 11:17:54 +0100

    Ajout du middleware sur get et ajout de la route de mise a jour de nom de prise

--- src/controller/routes/priseApiController.ts
+++ src/controller/routes/priseApiController.ts
@@ -3,11 +3,32 @@ import {prise} from "../../models/prise.model";
 
 export default class priseApiController extends controller{
     async index(){
-        let data;
-        data = await prise.query().select('id', 'mac', 'nom');
+        if(this.request.method === "GET"){
+            let data;
+            data = await prise.query().select('id', 'mac', 'nom');
 
-        this.response.json({
-            data: data
-        });
+            this.response.json({
+                data: data
+            });
+        }
+        else if(this.request.method == "POST"){
+            let {name, id} = this.request.body;
+            try{
+                let query = await prise.query().findById(id).patch({
+                    nom: name
+                });
+                this.response.json({
+                    success: true,
+                    updated: query
+                })
+            } catch (e) {
+                this.response.json({
+                    success: false,
+                    error: e
+                })
+            }
+
+        }
+
     }
 }
--- src/route.ts
+++ src/route.ts
@@ -7,6 +7,7 @@ export default [
     {path: "/api/logged", controller: "isLoggedController", type: "get"},
     {path: "/api/test", controller: 'apiTestController', type: "get"},
     {path: "/api/prise", controller: 'priseApiController', type: "get"},
+    {path: "/api/prise", controller: 'priseApiController', type: "post"},
     {path: "/api/humidite", controller: "humiditeApiController", type: "get"},
     {path: "/api/temperature", controller: "temperatureApiController", type: "get"},
     {path: "/api/watt", controller: "wattApiController", type: "get"},
