019c8a2b38ff9bc14a0c395ca53b5f10e37dbfe0
JEANTET Joey <jeantetjoey.pro@gmail.com>
2021-03-12 08:36:59 +0100

    Suppression des routes de tests

--- src/route.ts
+++ src/route.ts
@@ -5,7 +5,6 @@ export default [
     //{path: "/api", controller: "apiController", type: "get"},
     {path: "/api/login", controller: "loginController", type: "post"},
     {path: "/api/logged", controller: "isLoggedController", type: "get"},
-    {path: "/api/test", controller: 'apiTestController', type: "get"},
     {path: "/api/prise", controller: 'priseApiController', type: "get"},
     {path: "/api/prise", controller: 'priseApiController', type: "post"},
     {path: "/api/humidite", controller: "humiditeApiController", type: "get"},
