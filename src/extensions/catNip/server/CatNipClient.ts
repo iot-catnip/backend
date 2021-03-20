ac8c1d49ee4c5d81e87fe5d47c0d1c49931de6bf
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-03-20 19:57:29 +0100

    Feat : catnip serever everything works !

--- src/extensions/catNip/server/CatNipClient.ts
+++ src/extensions/catNip/server/CatNipClient.ts
@@ -1,30 +1,40 @@
 import {Socket} from "net";
 import CatNip from "../frame/CatNip";
+import {watt} from "../../../models/watt.model";
+import {prise} from "../../../models/prise.model";
+import {humidite} from "../../../models/humidite.model";
+import {temperature} from "../../../models/temperature.model";
+import CatNipServer from "./CatNipServer";
 
 export default class CatNipClient{
     private connexion: Socket;
     private mac: string | undefined;
+    private id: number | undefined;
     constructor(conn: Socket) {
         this.connexion = conn;
         this.connexion.on("data", data => this.data(data));
         this.connexion.on("close", data => this.close(data));
     }
 
-    private data(data: Buffer): void {
-        try{
+    private async data(data: Buffer): Promise<void> {
+        try {
             let catNip = new CatNip();
             catNip.decodeFrame(data);
-            if(this.mac == undefined) this.mac = catNip.getClientMacAddress;
-            console.log('query from: '+this.mac);
-            this.handler(catNip.getPacketType, data);
-
+            if (this.mac == undefined) {
+                this.mac = catNip.getClientMacAddress;
+                this.id = await this.getPlugIdFromMac(this.mac);
+            }
+            console.log(`[Info] > receive CatNipFarm type ${catNip.getPacketType?.toString(16)} from ${this.mac}`);
+            this.handler(catNip);
             this.sendRequest(CatNip.STATUS_RECEIVED);
-        }catch (e) {
+        } catch (e) {
             console.error(e);
         }
     }
 
     private close(donnees: Boolean): void {
+        if (this.id)
+            CatNipServer.removeConn(this.id)
         console.log("closed");
     }
 
@@ -40,8 +50,9 @@ export default class CatNipClient{
         }
     }
 
-    private sendRequest(type: number): void {
+    public sendRequest(type: number): void {
         try{
+            console.log()
             let catNip = new CatNip();
             catNip.setPacketType = type;
             catNip.encodeFrame();
@@ -51,44 +62,77 @@ export default class CatNipClient{
         }
     }
 
-    private handler(type: number | null, data: any){
-        switch (type){
+    private handler(catData: CatNip){
+        switch (catData?.getPacketType){
             case CatNip.DATA_CONSUMATION:
-                this.data_consumation(data);
+                this.data_consumation(catData.getData);
                 break;
             case CatNip.DATA_HUMIDITY:
-                this.data_humidity(data);
+                this.data_humidity(catData.getData);
                 break;
             case CatNip.DATA_TEMPERATURE:
-                this.data_temperature(data);
+                this.data_temperature(catData.getData);
                 break;
             case CatNip.DATA_ON:
-                this.data_on(data);
+                //@todo pas implementer sur la carte
+                //this.data_on(catData.getData);
                 break;
             case CatNip.STATUS_ALIVE:
-                this.data_alive(data);
+                //@todo traitement différant ici
+                //this.data_alive(data);
                 break;
         }
     }
 
     private data_consumation(data: any) {
-
+        if (this.id != undefined) {
+            const obj = {
+                prise_id:this.id,
+                date_mesure:Date.now(),
+                valeur:data
+            }
+            watt.query().insert(obj).execute()
+        }
     }
 
     private data_humidity(data: any) {
-
+        if (this.id != undefined){
+            const obj = {
+                prise_id:this.id,
+                date_mesure:Date.now(),
+                valeur:data
+            }
+            humidite.query().insert(obj).execute()
+        }
     }
 
     private data_temperature(data: any) {
-
+        if (this.id != undefined){
+            const obj = {
+                prise_id:this.id,
+                date_mesure:Date.now(),
+                valeur:data
+            }
+            temperature.query().insert(obj).execute();
+        }
     }
 
+    //@todo : pas implementer sur la carte car pas de relais
     private data_on(data: any) {
 
     }
 
     private data_alive(data: any) {
 
+    }
+
+    private async getPlugIdFromMac(mac: string): Promise<number> {
+        let plug = await prise.query().select('id').where('mac', '=', mac).limit(1).execute();
+        return parseInt(plug[0].$id());
+    }
+
+    get plugId():number|undefined{
+        return this.id;
     }
 
 }
--- src/extensions/catNip/server/CatNipServer.ts
+++ src/extensions/catNip/server/CatNipServer.ts
@@ -5,13 +5,56 @@ import SocketCollection from "./socketCollection";
 import CatNipClient from "./CatNipClient";
 
 export default class CatNipServer {
-    public static start(){
+    private static catNipClients: Array<CatNipClient> = [];
+    public static scheduleDelay: number;
+    public static activateScheduling: boolean;
+
+    public static start() {
         var server = createServer();
-        console.log("\x1b[32m[Info] > CATNIP Server Running",'\x1b[0m');
+        console.log("\x1b[32m[Info] > CATNIP Server Running", '\x1b[0m');
         server.on('connection', conn => {
-            new CatNipClient(conn);
+            CatNipServer.catNipClients.push(new CatNipClient(conn));
         });
         server.listen(7788);
+        CatNipServer.activateScheduling = true
+        CatNipServer.scheduleDelay = 50000;
+        CatNipServer.schedule();
+    }
+
+    private static schedule() {
+        if (CatNipServer.activateScheduling) {
+            CatNipServer.catNipClients.forEach(value => {
+                if (value instanceof CatNipClient) {
+
+                    console.log("Ask Temperature")
+                    value.sendRequest(CatNip.ASK_TEMPERATURE);
+
+                    setTimeout(() => {
+
+                        console.log("Ask Humidity")
+                        value.sendRequest(CatNip.ASK_HUMIDITY);
+
+                        setTimeout(() => {
+
+                            console.log("Ask watt")
+                            value.sendRequest(CatNip.ASK_WATT);
+
+                        }, CatNipServer.scheduleDelay / 3);
+                    }, CatNipServer.scheduleDelay / 3);
+                }
+            })
+            setTimeout(() => {
+                CatNipServer.schedule()
+            }, CatNipServer.scheduleDelay)
+        }
+    }
+
+    static removeConn(id : number) {
+        for (let i = 0; i < CatNipServer.catNipClients.length; i++) {
+            if (CatNipServer.catNipClients[i].plugId===id){
+                CatNipServer.catNipClients.splice(i,1);
+            }
+        }
     }
 }
 
